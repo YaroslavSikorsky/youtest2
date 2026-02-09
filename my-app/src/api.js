61f460f6bc5c621f3a7043cc4a8de1c3ec5831f0
S <yrl.sikorsky@gmail.com>
2026-02-09 21:44:02 +0300

    add fix dep front 4

--- my-app/src/api.js
+++ my-app/src/api.js
@@ -3,7 +3,9 @@
 // Используем переменную окружения для URL API
 // В разработке: http://localhost:8087
 // В продакшене: значение из VITE_API_URL
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8087";
+// export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8087";
+export const API_URL = 'https://youtest2.railway.internal';
+
 
 console.log("API URL:", API_URL); // Для отладки
 
