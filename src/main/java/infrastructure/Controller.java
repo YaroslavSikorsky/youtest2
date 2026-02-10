d09068691f76f563bc0a8af7fa38e16e413cef20
S <yrl.sikorsky@gmail.com>
2026-02-10 14:39:19 +0300

    add fix cors 10

--- src/main/java/infrastructure/Controller.java
+++ src/main/java/infrastructure/Controller.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-//@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/notes")
 public class Controller {
