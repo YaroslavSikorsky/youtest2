e8589f4f77028eeaea52b53c43ee1506ab1b792c
S <yrl.sikorsky@gmail.com>
2026-02-11 13:24:59 +0300

    add fix cors 24, rework cors conf add WebMvcConfigurer

--- src/main/java/infrastructure/CorsConfig.java
+++ src/main/java/infrastructure/CorsConfig.java
@@ -1,45 +1,24 @@
 package infrastructure;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Value("${cors.allowed.origins}")
     private String frontend;
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-
-        // Для дебага
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
         System.out.println("CORS frontend: [" + frontend + "]");
 
-        // Разрешаем фронт (обязательно без лишних пробелов/кавычек)
-        config.setAllowedOrigins(Arrays.asList(frontend.trim()));
-
-        // Методы
-        config.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
-
-        // Заголовки
-        config.setAllowedHeaders(Arrays.asList("*"));
-
-        // Если нужны куки/сессии
-        config.setAllowCredentials(true);
-
-        // Expose для JWT или других нужных заголовков
-        config.addExposedHeader("Authorization");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return source;
+        registry.addMapping("/**")
+                .allowedOrigins(frontend.trim())
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
